238ed3a286222a37f25455794ea4649391463704
Richard Stock <coding.rstock@gmail.com>
2020-09-04 20:04:39 +0000

    Changes

---
+++ config.rb
@@ -0,0 +1,15 @@
+# GOLLUM CONFIG #
+# ---------------
+
+# always include this:
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# first remove all markups:
+Gollum::Markup.formats.clear
+
+# and then define the sole markup to be supported:
+Gollum::Markup.formats[:markdown] = {
+    :name => "Markdown",
+    :extensions => ['wiki'],
+    :reverse_links => false
+}
